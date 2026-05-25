f2b5afe7c31c57755685b7a7c2d526602e53dff0
Sebastian <u.cuevas.5ddb@gmail.com>
2026-05-25 00:27:54 -0600

    Procfile 8

--- skyship-express/backend/application.py
+++ skyship-express/backend/application.py
@@ -1,1 +1,10 @@
-from run import app as application
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from app import create_app, db
+
+application = create_app()
+
+with application.app_context():
+    db.create_all()
