df1cd4170c8406cf73cb497ac5d4055d47b42c2e
Sebastian <u.cuevas.5ddb@gmail.com>
2026-05-24 22:33:04 -0600

    Procfile 4

--- skyship-express/backend/run.py
+++ skyship-express/backend/run.py
@@ -7,11 +7,13 @@ from app import create_app, db
 
 app = create_app()
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        print("✅ Base de datos inicializada correctamente.")
+# Al poner esto aquí afuera, Gunicorn lo ejecutará al arrancar en AWS
+with app.app_context():
+    db.create_all()
+    print("✅ Base de datos inicializada (o verificada) correctamente.")
 
+# Esto solo se ejecutará cuando corras el servidor en tu computadora (local)
+if __name__ == '__main__':
     port = int(os.getenv('PORT', 8000))
     debug = os.getenv('FLASK_ENV', 'development') == 'development'
     print(f"🚀 SkyShip Express API corriendo en http://localhost:{port}")
